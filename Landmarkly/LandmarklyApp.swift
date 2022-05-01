4d6896366522d00e6cee4aca32fad58734bdcc8b
Мугинов Артур <skinxcheg>
2022-05-01 23:35:45 +0500

    Initial Commit

---
+++ Landmarkly/LandmarklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarklyApp.swift
+//  Landmarkly
+//
+//  Created by Артур Мугинов on 01.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct LandmarklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
